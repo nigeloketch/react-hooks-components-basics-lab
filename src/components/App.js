e0a5a5578867d5f8ba6972eaa6b591b62684027f
Nigel <nigel.oketch@student.moringaschool.com>
2023-07-21 01:43:40 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 function NavBar() {
   return (
     <nav>
@@ -7,7 +6,33 @@ function NavBar() {
     </nav>
   );
 }
-
+function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
+    </div> )} 
+import React from "react";
+function NavBar(){ 
+} {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
+    </div>) }
+import React from "react";
+function NavBar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
 function Home() {
   return (
     <div id="home">
@@ -16,15 +41,22 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id= 'about'>
+      Lego I'm creating!
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      {/* write an <About> component here */}
+      {<Home />}
+      {<About />}
     </div>
   );
 }
